2d603a83254a7c1dfad29a76e0d777822ef26316
Vladimir Gladkov <vovkos@gmail.com>
2016-10-28 15:03:11 +0000

    initial commit

---
+++ dependencies.cmake
@@ -0,0 +1,34 @@
+#...............................................................................
+#
+#  This file is part of the Doxyrest toolkit.
+#
+#  Doxyrest is distributed under the MIT license.
+#  For details see accompanying license.txt file,
+#  the public copy of which is also available at:
+#  http://tibbo.com/downloads/archive/doxyrest/license.txt
+#
+#...............................................................................
+
+set (
+	AXL_PATH_LIST
+
+	LUA_INC_DIR
+	LUA_LIB_DIR
+	EXPAT_INC_DIR
+	EXPAT_LIB_DIR
+	7Z_EXE
+	RAGEL_EXE
+	)
+
+set (
+	AXL_IMPORT_LIST
+
+	REQUIRED
+		expat
+		lua
+	OPTIONAL
+		7z
+		ragel
+	)
+
+#...............................................................................
