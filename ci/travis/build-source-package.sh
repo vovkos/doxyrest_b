d86ddcc53f1bdd93bb069077538881a21e65f0e6
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2019-08-15 19:53:32 +0300

    [ci] update build-source-package.sh (make it sourceable)

--- ci/travis/build-source-package.sh
+++ ci/travis/build-source-package.sh
@@ -13,21 +13,20 @@ if  [ "$TRAVIS_TAG" != "" ] && \
 	[ "$TRAVIS_OS_NAME" == "linux" ] && \
 	[ "$BUILD_CONFIGURATION" == "Release" ] && \
 	[ "$CC" == "gcc" ]; then
+
+	# build package first
+
+	cpack --config CPackSourceConfig.cmake
+
+	# then get package file name
+
+	echo 'include(build/CPackSourceConfig.cmake)' >  print-package-file-name.cmake
+	echo 'message(${CPACK_PACKAGE_FILE_NAME})'    >> print-package-file-name.cmake
+
+	CPACK_PACKAGE_FILE_NAME=`cmake -P print-package-file-name.cmake 2>&1`
+
+	rm print-package-file-name.cmake
+
+	export DOXYREST_SOURCE_PACKAGE_FILE=build/$CPACK_PACKAGE_FILE_NAME.tar.xz
 	export BUILD_SOURCE_PACKAGE=ON
-else
-	return
 fi
-
-# build package
-
-cpack --config CPackSourceConfig.cmake
-
-# get package file name
-
-echo 'include(build/CPackSourceConfig.cmake)' >  print-package-file-name.cmake
-echo 'message(${CPACK_PACKAGE_FILE_NAME})'    >> print-package-file-name.cmake
-
-CPACK_PACKAGE_FILE_NAME=`cmake -P print-package-file-name.cmake 2>&1`
-export DOXYREST_SOURCE_PACKAGE_FILE=build/$CPACK_PACKAGE_FILE_NAME.tar.xz
-
-rm print-package-file-name.cmake
